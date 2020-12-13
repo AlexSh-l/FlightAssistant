2b1b9a3ef87322264ed34c3ae271b2c33b5dd7a0
ashust75@gmail.com <ashust75@gmail.com>
2020-12-13 15:01:16 +0300

    Servlet

--- src/main/java/by/alx/flightassist/controller/Controller.java
+++ src/main/java/by/alx/flightassist/controller/Controller.java
@@ -1,4 +1,22 @@
 package by.alx.flightassist.controller;
 
-public class Controller {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/controller")
+public class Controller extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        //request.setAttribute("name", "Tom");
+        //request.setAttribute("age", 34);
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+        //getServletContext().getRequestDispatcher("/basic.jsp").forward(request, response);
+
+    }
 }
